ddc9f3f2f8b3c7039e6d863c41f7559334d1457d
Hazel <yapsy@sea.com>
2024-04-20 20:30:33 +0800

    Updated logo

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -2,22 +2,26 @@ package ui;
 
 import java.util.Scanner;
 
+/**
+* Manages the user interface (which is a CLI) which is displayed.
+* Includes various helper functions for displaying messages or prompts to the user.
+*/
 public class Ui {
 
     public void printLogo(){
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "    _               _     \n"
+                + "   | |__     ___   | |__  \n"
+                + "   | '_ \\   / _ \\  | '_ \\ \n"
+                + "   | |_) | | (_) | | |_) |\n"
+                + "   |_.__/   \\___/  |_.__/ \n";
         System.out.println(logo);
     }
     public void printLoadingError(){
-        System.out.println("Error loading tasks from file.");
+        System.out.println("Error loading tasks from file. D:");
     }
 
     public void printHello() {
-        System.out.println("Hello! I'm Bob\nWhat can I do for you?");
+        System.out.println("Hello! I'm Bob :D\nWhat can I do for you?");
     }
 
     public String readCommand(){
